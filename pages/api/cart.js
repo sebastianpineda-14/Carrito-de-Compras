fd4e04359123d913d3f0f336dd3c3d1ae3b551dd
Sebastián Pineda <sebastianpineda.14@outlook.com>
2025-08-15 21:21:13 -0500

    Carga modificacion style

--- pages/api/cart.js
+++ pages/api/cart.js
@@ -1,9 +1,9 @@
 let cart = [];
 
 const products = [
-  { id: 1, name: "Producto 1", price: 100 },
-  { id: 2, name: "Producto 2", price: 200 },
-  { id: 3, name: "Producto 3", price: 60 },
+  { id: 1, name: "Producto 1", price: 60 },
+  { id: 2, name: "Producto 2", price: 100 },
+  { id: 3, name: "Producto 3", price: 120 },
   { id: 4, name: "Producto 4", price: 70 }
 ];
 
--- pages/api/products.js
+++ pages/api/products.js
@@ -1,8 +1,8 @@
 export default function handler(req, res) {
   const products = [
-    { id: 1, name: "Producto 1", price: 100 },
-    { id: 2, name: "Producto 2", price: 200 },
-    { id: 3, name: "Producto 3", price: 60 },
+    { id: 1, name: "Producto 1", price: 60 },
+    { id: 2, name: "Producto 2", price: 100 },
+    { id: 3, name: "Producto 3", price: 120 },
     { id: 4, name: "Producto 4", price: 70 },
   ];
   res.status(200).json(products);
